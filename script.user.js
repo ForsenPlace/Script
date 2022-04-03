e5fb6328b98682194988811efd14f1fef753afda
ForsenPlace <forsenplace@gmail.com>
2022-04-03 21:13:45 +0200

    map expanded again

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ForsenPlace Script
 // @namespace    https://github.com/ForsenPlace/Script
-// @version      13
+// @version      14
 // @description  Script 
 // @author       ForsenPlace
 // @match        https://www.reddit.com/r/place/*
@@ -26,53 +26,69 @@ const CHECK_AGAIN_DELAY = 30 * 1000
 const REFRESH_TOKEN_DELAY = 30 * 60 * 1000
 
 const COLOR_TO_INDEX = {
+	'#6D001A': 0,
 	'#BE0039': 1,
 	'#FF4500': 2,
 	'#FFA800': 3,
 	'#FFD635': 4,
+	'#FFF8B8': 5,
 	'#00A368': 6,
 	'#00CC78': 7,
 	'#7EED56': 8,
 	'#00756F': 9,
 	'#009EAA': 10,
+	'#00CCC0': 11,
 	'#2450A4': 12,
 	'#3690EA': 13,
 	'#51E9F4': 14,
 	'#493AC1': 15,
 	'#6A5CFF': 16,
+	'#94B3FF': 17,
 	'#811E9F': 18,
 	'#B44AC0': 19,
+	'#E4ABFF': 20,
+	'#DE107F': 21,
 	'#FF3881': 22,
 	'#FF99AA': 23,
 	'#6D482F': 24,
 	'#9C6926': 25,
+	'#FFB470': 26,
 	'#000000': 27,
+	'#515252': 28,
 	'#898D90': 29,
 	'#D4D7D9': 30,
 	'#FFFFFF': 31
 };
 const INDEX_TO_NAME = {
+	'0': 'burgundy',
 	'1': 'dark red',
 	'2': 'red',
 	'3': 'orange',
 	'4': 'yellow',
+	'5': 'pale yellow',
 	'6': 'dark green',
 	'7': 'green',
 	'8': 'light green',
 	'9': 'dark teal',
 	'10': 'teal',
+	'11': 'light teal',
 	'12': 'dark blue',
 	'13': 'blue',
 	'14': 'light blue',
 	'15': 'indigo',
 	'16': 'periwinkle',
+	'17': 'lavender',
 	'18': 'dark purple',
 	'19': 'purple',
+	'20': 'pale purple',
+	'21': 'magenta',
 	'22': 'pink',
 	'23': 'light pink',
 	'24': 'dark brown',
 	'25': 'brown', 
+	'26': 'beige', 
 	'27': 'black',
+	'28': 'dark gray',
 	'29': 'gray',
 	'30': 'light gray',
 	'31': 'white'
@@ -85,7 +101,7 @@ var canvas = document.createElement('canvas');
 (async function () {
 	GM_addStyle(GM_getResourceText('TOASTIFY_CSS'));
 	canvas.width = 2000;
-	canvas.height = 1000;
+	canvas.height = 2000;
 	canvas.style.display = 'none';
 	canvas = document.body.appendChild(canvas);
 
@@ -150,6 +166,8 @@ async function executeOrders() {
 	try {
 		ctx = await getCanvasFromUrl(await getCurrentImageUrl('0'), 0, 0);
 		ctx = await getCanvasFromUrl(await getCurrentImageUrl('1'), 1000, 0);
+		ctx = await getCanvasFromUrl(await getCurrentImageUrl('2'), 0, 1000);
+		ctx = await getCanvasFromUrl(await getCurrentImageUrl('3'), 1000, 1000);
 	} catch (e) {
 		console.warn('Error obtaining map', e);
 		Toastify({
